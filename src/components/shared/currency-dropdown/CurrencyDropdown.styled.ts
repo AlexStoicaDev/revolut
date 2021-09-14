fb8b0ddb6782595d8781fd91e9799d53aebdafa3
Alexandru Stoica <petrutstoica123@yahoo.com>
2021-09-14 23:16:01 +0300

    Move CurrencyDropdown styled components in sperate file

---
+++ src/components/shared/currency-dropdown/CurrencyDropdown.styled.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const DropDownContainer = styled.div`
+    width: 100px;
+`;
+
+export const DropDownHeader = styled.div`
+    cursor: pointer;
+    font-weight: bold;
+    font-size: 20px;
+    color: ${({ theme }) => theme.colors.main};
+    background: #ffffff;
+
+    & > * {
+        vertical-align: middle;
+    }
+    & > svg {
+        margin-top: 4px;
+    }
+`;
+
+export const DropDownListContainer = styled.div`
+    position: absolute;
+    text-align: left;
+    width: 100px;
+`;
+
+export const DropDownList = styled.ul`
+    padding: 0 0 0 12px;
+    margin: 10px 0 0 0;
+    background: ${({ theme }) => theme.colors.white};
+    box-shadow: 2px 2px 4px rgb(228, 228, 228);
+    border-radius: 10px;
+    box-sizing: border-box;
+    color: ${({ theme }) => theme.colors.grey};
+    font-size: 20px;
+    font-weight: 500;
+    &:first-child {
+        padding-top: 12px;
+    }
+`;
+
+export const ListItem = styled.li`
+    list-style: none;
+    margin-bottom: 12px;
+    cursor: pointer;
+    &:hover {
+        color: ${({ theme }) => theme.colors.main};
+    }
+`;
--- src/components/shared/currency-dropdown/CurrencyDropdown.tsx
+++ src/components/shared/currency-dropdown/CurrencyDropdown.tsx
@@ -1,57 +1,13 @@
 import { useState } from 'react';
-import styled from 'styled-components';
 import { RiArrowDownSLine } from 'react-icons/ri';
 import { Currency } from '../../../models/Currency';
-
-const DropDownContainer = styled.div`
-    width: 100px;
-    margin: 0 auto;
-`;
-
-const DropDownHeader = styled.div`
-    cursor: pointer;
-    font-weight: bold;
-    font-size: 20px;
-    color: ${({ theme }) => theme.colors.main};
-    background: #ffffff;
-
-    & > * {
-        vertical-align: middle;
-    }
-    & > svg {
-        margin-top: 4px;
-    }
-`;
-
-const DropDownListContainer = styled.div`
-    position: absolute;
-    text-align: left;
-    width: 100px;
-`;
-
-const DropDownList = styled.ul`
-    padding: 0 0 0 12px;
-    margin: 10px 0 0 0;
-    background: ${({ theme }) => theme.colors.white};
-    box-shadow: 2px 2px 4px rgb(228, 228, 228);
-    border-radius: 10px;
-    box-sizing: border-box;
-    color: ${({ theme }) => theme.colors.grey};
-    font-size: 20px;
-    font-weight: 500;
-    &:first-child {
-        padding-top: 12px;
-    }
-`;
-
-const ListItem = styled.li`
-    list-style: none;
-    margin-bottom: 12px;
-    cursor: pointer;
-    &:hover {
-        color: ${({ theme }) => theme.colors.main};
-    }
-`;
+import {
+    DropDownContainer,
+    DropDownHeader,
+    DropDownList,
+    DropDownListContainer,
+    ListItem,
+} from './CurrencyDropdown.styled';
 
 interface CurrencyDropdownProps {
     selectedCurrency: Currency;
