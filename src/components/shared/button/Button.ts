aa071dd44683468ec945017d320a19711dbc7216
Alexandru Stoica <petrutstoica123@yahoo.com>
2021-09-14 20:54:34 +0300

    Create shared button styled-component

---
+++ src/components/shared/button/Button.ts
@@ -0,0 +1,26 @@
+import styled, { css } from 'styled-components';
+
+const Button = styled.button`
+    border-radius: 15px;
+    color: ${({ theme }) => theme.colors.white};
+    font-family: 'Effra', sans-serif;
+    background-color: ${({ theme, disabled }) =>
+        disabled ? theme.colors.inactive : theme.colors.accent};
+    padding: 12px 25px;
+    font-size: 18px;
+    outline: none;
+    cursor: pointer;
+    border: none;
+
+    & > * {
+        vertical-align: middle;
+    }
+
+    ${({ disabled }) =>
+        !disabled &&
+        css`
+            box-shadow: 0px 4px 6px rgb(131 164 247);
+        `}
+`;
+
+export default Button;
