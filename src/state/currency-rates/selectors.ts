a2bb9e4b10f76b9d51258fb6351ae64f57d8f496
AlexStoicaDev <petrutstoica123@yahoo.com>
2021-09-17 03:15:03 +0300

    Display numbers with only 2 decimal places for rate and input values

--- src/state/currency-rates/selectors.ts
+++ src/state/currency-rates/selectors.ts
@@ -1,8 +1,9 @@
+import { getNumberWithTwoDecimals } from '../../utils/exchange';
 import { State } from '../state';
 
 export function selectExchangeRateForCurrency(
     currencyCode: string,
     state: State,
 ): number | undefined {
-    return state.currencyRates.conversionRates[currencyCode];
+    return getNumberWithTwoDecimals(state.currencyRates.conversionRates[currencyCode]);
 }
--- src/utils/exchange.tsx
+++ src/utils/exchange.tsx
@@ -1,3 +1,7 @@
 export const getExchangedValue = (value: number, exchangeRate?: number): number => {
-    return value * (exchangeRate || 1);
+    return getNumberWithTwoDecimals(value * (exchangeRate || 1));
+};
+
+export const getNumberWithTwoDecimals = (value: number): number => {
+    return Number(Math.round(parseFloat(value + 'e' + 2)) + 'e-' + 2);
 };
